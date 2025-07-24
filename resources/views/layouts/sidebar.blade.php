2b0eb31dff63e9449cde653b1e47aee7fffb002c
anasnh12 <anasnurhidayat70@gmail.com>
2025-07-25 01:56:21 +0700

    update sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -77,87 +77,86 @@
     </li>
     @endif
 
-    <!-- Menu Admin -->
-    @if(Auth::user()->id_level == 1)
-    <li class="nav-item {{ in_array($activeMenu, ['pegawai','presensi-admin','jabatan','riwayat-jabatan','pangkat','golongan','kgp','riwayat-gaji','pendidikan','laporan-gaji','cuti']) ? 'menu-open' : '' }}">
-      <a href="#" class="nav-link {{ in_array($activeMenu, ['pegawai','presensi-admin','jabatan','riwayat-jabatan','pangkat','golongan','kgp','riwayat-gaji','pendidikan','laporan-gaji','cuti']) ? 'active' : '' }}">
-        <i class="nav-icon fas fa-user-shield"></i>
-        <p>
-          Manajemen Kepegawaian
-          <i class="right fas fa-angle-left"></i>
-        </p>
+<!-- Menu Admin -->
+@if(Auth::user()->id_level == 1)
+<li class="nav-item {{ in_array($activeMenu, ['pegawai','presensi-admin','cuti','jabatan','riwayat-jabatan','pangkat','golongan','kgp','riwayat-gaji','pendidikan','laporan-gaji']) ? 'menu-open' : '' }}">
+  <a href="#" class="nav-link {{ in_array($activeMenu, ['pegawai','presensi-admin','cuti','jabatan','riwayat-jabatan','pangkat','golongan','kgp','riwayat-gaji','pendidikan','laporan-gaji']) ? 'active' : '' }}">
+    <i class="nav-icon fas fa-user-shield"></i>
+    <p>
+      Manajemen Kepegawaian
+      <i class="right fas fa-angle-left"></i>
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="{{ url('/pegawai') }}" class="nav-link {{ ($activeMenu == 'pegawai') ? 'active' : '' }}">
+        <i class="fas fa-id-badge nav-icon"></i>
+        <p>Data Pegawai</p>
       </a>
-      <ul class="nav nav-treeview">
-        <li class="nav-item">
-          <a href="{{ url('/pegawai') }}" class="nav-link {{ ($activeMenu == 'pegawai') ? 'active' : '' }}">
-            <i class="fas fa-id-badge nav-icon"></i>
-            <p>Data Pegawai</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="{{ url('/presensi-admin') }}" class="nav-link {{ ($activeMenu == 'presensi-admin') ? 'active' : '' }}">
-            <i class="fas fa-fingerprint nav-icon"></i>
-            <p>Data Presensi</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="{{ url('/jabatan') }}" class="nav-link {{ ($activeMenu == 'jabatan') ? 'active' : '' }}">
-            <i class="fas fa-briefcase nav-icon"></i>
-            <p>Data Jabatan</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="{{ url('/riwayat-jabatan') }}" class="nav-link {{ ($activeMenu == 'riwayat-jabatan') ? 'active' : '' }}">
-            <i class="fas fa-history nav-icon"></i>
-            <p>Riwayat Jabatan</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="{{ url('/pangkat') }}" class="nav-link {{ ($activeMenu == 'pangkat') ? 'active' : '' }}">
-            <i class="fas fa-signal nav-icon"></i>
-            <p>Pangkat</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="{{ url('/golongan') }}" class="nav-link {{ ($activeMenu == 'golongan') ? 'active' : '' }}">
-            <i class="fas fa-layer-group nav-icon"></i>
-            <p>Golongan Pangkat</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="{{ url('/kgp') }}" class="nav-link {{ ($activeMenu == 'kgp') ? 'active' : '' }}">
-            <i class="fas fa-chart-line nav-icon"></i>
-            <p>Kenaikan Gaji Berkala</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="{{ url('/riwayat-gaji') }}" class="nav-link {{ ($activeMenu == 'riwayat-gaji') ? 'active' : '' }}">
-            <i class="fas fa-money-bill-wave nav-icon"></i>
-            <p>Riwayat Gaji</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="{{ url('/pendidikan') }}" class="nav-link {{ ($activeMenu == 'pendidikan') ? 'active' : '' }}">
-            <i class="fas fa-graduation-cap nav-icon"></i>
-            <p>Pendidikan</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="{{ url('/laporan-gaji') }}" class="nav-link {{ ($activeMenu == 'laporan-gaji') ? 'active' : '' }}">
-            <i class="fas fa-file-invoice-dollar nav-icon"></i>
-            <p>Laporan Gaji</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="{{ url('/cutiadmin') }}" class="nav-link {{ ($activeMenu == 'cuti') ? 'active' : '' }}">
-            <i class="fas fa-calendar-alt nav-icon text-danger"></i>
-            <p>Manajemen Cuti</p>
-          </a>
-        </li>
-      </ul>
     </li>
-    @endif
-
+    <li class="nav-item">
+      <a href="{{ url('/presensi-admin') }}" class="nav-link {{ ($activeMenu == 'presensi-admin') ? 'active' : '' }}">
+        <i class="fas fa-fingerprint nav-icon"></i>
+        <p>Data Presensi</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="{{ url('/cutiadmin') }}" class="nav-link {{ ($activeMenu == 'cuti') ? 'active' : '' }}">
+        <i class="fas fa-calendar-alt nav-icon text-danger"></i>
+        <p>Manajemen Cuti</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="{{ url('/jabatan') }}" class="nav-link {{ ($activeMenu == 'jabatan') ? 'active' : '' }}">
+        <i class="fas fa-briefcase nav-icon"></i>
+        <p>Data Jabatan</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="{{ url('/riwayat-jabatan') }}" class="nav-link {{ ($activeMenu == 'riwayat-jabatan') ? 'active' : '' }}">
+        <i class="fas fa-history nav-icon"></i>
+        <p>Riwayat Jabatan</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="{{ url('/pangkat') }}" class="nav-link {{ ($activeMenu == 'pangkat') ? 'active' : '' }}">
+        <i class="fas fa-signal nav-icon"></i>
+        <p>Pangkat</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="{{ url('/golongan') }}" class="nav-link {{ ($activeMenu == 'golongan') ? 'active' : '' }}">
+        <i class="fas fa-layer-group nav-icon"></i>
+        <p>Golongan Pangkat</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="{{ url('/kgp') }}" class="nav-link {{ ($activeMenu == 'kgp') ? 'active' : '' }}">
+        <i class="fas fa-chart-line nav-icon"></i>
+        <p>Kenaikan Gaji Berkala</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="{{ url('/riwayat-gaji') }}" class="nav-link {{ ($activeMenu == 'riwayat-gaji') ? 'active' : '' }}">
+        <i class="fas fa-money-bill-wave nav-icon"></i>
+        <p>Riwayat Gaji</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="{{ url('/pendidikan') }}" class="nav-link {{ ($activeMenu == 'pendidikan') ? 'active' : '' }}">
+        <i class="fas fa-graduation-cap nav-icon"></i>
+        <p>Pendidikan</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="{{ url('/laporan-gaji') }}" class="nav-link {{ ($activeMenu == 'laporan-gaji') ? 'active' : '' }}">
+        <i class="fas fa-file-invoice-dollar nav-icon"></i>
+        <p>Laporan Gaji</p>
+      </a>
+    </li>
+  </ul>
+</li>
+@endif
   </ul>
 </nav>
 <!-- End Sidebar -->
